a8b205f651fcb242be234cc4466171cfbc8f732a
marko <markok.kod@gmail.com>
2026-03-16 12:58:07 +0100

    Test commit

--- javascript.js
+++ javascript.js
@@ -1,8 +1,3 @@
-let containerTable = document.getElementById("table");
 
 
-for (let i = 0; i < 16*16; i++) {
-    let inidividualDiv = document.createElement("div");
-    inidividualDiv.setAttribute("class", "tile");
-    containerTable.appendChild(inidividualDiv);
-}
+

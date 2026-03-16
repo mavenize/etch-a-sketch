9b88e3e81be6e13d4061395855f87af4b6fafcdc
marko <markok.kod@gmail.com>
2026-03-16 13:10:09 +0100

    Added individual tiles

--- javascript.js
+++ javascript.js
@@ -1,3 +1,10 @@
 
+const Container = document.getElementById("table");
+
+for(i=0 ; i < 16*16; i++) {
+    individualDiv = document.createElement("div");
+    individualDiv.setAttribute("class", "tile");;
+    Container.appendChild(individualDiv);
+}
 
 
